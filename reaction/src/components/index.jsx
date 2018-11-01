b60313685786b9a5b11984c6bfe4c9964bffdba5
Jayleh <jaylehyun@gmail.com>
2018-11-01 16:50:06 -0700

    refactoring portfolio to react

---
+++ reaction/src/components/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Portfolio extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello World</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Portfolio;
