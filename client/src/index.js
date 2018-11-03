9ffe8482385aa031e4bf7db70520fb66e27c45a5
Jayleh <jaylehyun@gmail.com>
2018-11-02 21:59:51 -0700

    moving App.jsx to componenets folder

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import 'leaflet/dist/leaflet.css';
 
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
